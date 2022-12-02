7b117113083ea24d53f2eb139c6e3ee7507e4fda
Álvaro Pérez <alvarojperezperez@gmail.com>
2022-12-02 13:52:03 +0100

    Add test endpoints

--- api/index.py
+++ api/index.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route('/')
-def root():
-    return "Hello World!"
+artists = [
+]
+
+@app.route('/artists')
+def get_artists():
+    return jsonify(artists)
+
+@app.route('/artists', methods=['POST'])
+def add_artist():
+    artists.append(request.get_json())
+    return '', 204
