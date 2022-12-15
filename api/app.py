892629770076d8f4c38fde0b01642060f1f66465
Álvaro Pérez <alvarojperezperez@gmail.com>
2022-12-16 00:28:00 +0100

    Add CRUD operations for artists

---
+++ api/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify, request
+from marshmallow.exceptions import ValidationError
+from api.models import db, ma
+from api.models.artist import ArtistModel, ArtistSchema
+
+
+app = Flask(__name__)
+app.config.from_pyfile("config.cfg")
+
+db.init_app(app)
+ma.init_app(app)
+
+@app.route('/artists')
+def get_artists():
+    all_artists = ArtistModel.query.all()
+    return jsonify(ArtistSchema(many=True).dump(all_artists))
+
+@app.route('/artists', methods=['POST'])
+def create_artist():
+    try:
+        artist = ArtistSchema().load(request.json)
+    except ValidationError as err:
+        return err.messages, 400
+
+    db.session.add(artist)
+    db.session.commit()
+    return ArtistSchema().jsonify(artist), 201
+
+@app.route('/artists/<int:artist_id>', methods=['PATCH'])
+def modify_artist(artist_id):
+    artist = ArtistSchema().load(
+        request.json,
+        instance=ArtistModel.query.get(artist_id),
+        partial=True)
+
+    if artist.artist_id is None:
+        return '', 404
+
+    db.session.commit()
+    return ArtistSchema().jsonify(artist), 200
+
+@app.route('/artists/<int:artist_id>', methods=['DELETE'])
+def delete_artist(artist_id):
+    artist = ArtistModel.query.get(artist_id)
+    if artist is None:
+        return '', 404
+
+    db.session.delete(artist)
+    db.session.commit()
+    return ArtistSchema().jsonify(artist), 200
